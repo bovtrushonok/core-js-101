6c6ea66845605fcc301fe4796144ba3bda5b1e56
Marie Bovtramovich <m.abovt@mail.ru>
2020-12-13 19:30:04 +0300

    add: date tasks

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return Date.parse(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return Date.parse(value);
 }
 
 
@@ -53,8 +53,19 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  let result;
+  if (date.getFullYear() % 4 === 0 && date.getFullYear() !== 1900) {
+    if ((date.getFullYear() % 100 === 0 && date.getFullYear() % 400 === 0)
+    || (date.getFullYear() % 100 !== 0)) {
+      result = true;
+      return result;
+    }
+  } else {
+    result = false;
+    return result;
+  }
+  return result;
 }
 
 
@@ -73,8 +84,27 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const difference = endDate - startDate;
+  const mSec = difference % 1000;
+  const diffSec = parseInt(difference / 1000, 10) % 60;
+  const diffMin = parseInt(difference / 60000, 10) % 60;
+  const diffHour = parseInt(difference / 3600000, 10);
+
+  const hour = diffHour < 10 ? `0${diffHour}` : diffHour;
+  const minutes = diffMin < 10 ? `0${diffMin}` : diffMin;
+  const sec = diffSec < 10 ? `0${diffSec}` : diffSec;
+  let ms;
+
+  if (mSec < 10) {
+    ms = `00${mSec}`;
+  } else if (mSec < 100) {
+    ms = `0${mSec}`;
+  } else {
+    ms = mSec;
+  }
+
+  return `${hour}:${minutes}:${sec}.${ms}`;
 }
 
 
