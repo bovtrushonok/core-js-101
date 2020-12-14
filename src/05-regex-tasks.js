118be2214180fb2060528456f06be534c49a9dcb
Marie Bovtramovich <m.abovt@mail.ru>
2020-12-14 18:35:09 +0300

    regexp for guid

--- src/05-regex-tasks.js
+++ src/05-regex-tasks.js
@@ -32,7 +32,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-  return /{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}/g;
+  return /{[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}}/;
 }
 
 
